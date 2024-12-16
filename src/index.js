89795213e86b76231af3042e62cf6fa8487116e5
lucas paulina <lucas.paulina04@gmail.com>
2024-12-15 22:26:33 -0300

    Se realiza modificación de titulo

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+
+// Establecer el título de la pestaña
+document.title = 'Holding Triple'; // Cambia aquí el nombre
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -9,6 +13,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Si deseas medir el rendimiento, puedes pasar una función como se muestra abajo
+// reportWebVitals(console.log);
